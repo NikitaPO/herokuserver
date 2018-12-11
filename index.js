181ae2ff421d981e30b25626e485f4c984180dc5
NikitaPO <gigzim345@gmail.com>
2018-12-11 10:02:30 +0300

    1

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ require('http')
         res.end('Hello world!');
         break;
       }
-      case: '/add?': {
-        let n1, n2;
-        ({n1, n2} = require('querystring').parse(u.substring(5)));
-        res.end('n1 + n2 = ', n1 + n2);
-        break;
-      }
+      
       default: res.end('Hi! I am working');
     }
   })
