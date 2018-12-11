685128f22f5358faa8b8b1b3cf7fc3a07dd299b2
NikitaPO <gigzim345@gmail.com>
2018-12-11 09:44:37 +0300

    add querystring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PORT = 4321;
 require('http')
-.Server(({url: u}, res) => {
-res.end('Hello, world!');
-})
-.listen(process.env.PORT || PORT);
+  .Server(({url: u}, res) => {
+    res.end('Hello, world!');
+  })
+  .listen(process.env.PORT || PORT);
