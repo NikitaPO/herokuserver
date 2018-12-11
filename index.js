5147a0ee050c8c458dd2b5d22771dee860a154ee
NikitaPO <gigzim345@gmail.com>
2018-12-11 10:30:18 +0300

    End?

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('http')
         if(u.startsWith('/add?')){
           let n1, n2;
           ({ n1, n2 } = require('querystring').parse(u.substring(5)));
-          res.end('n1 + n2 = ', (Number(n1) + Number(n2)));
+          res.end('n1 + n2 = ' + (Number(n1) + Number(n2)));
         } else res.end('Hi! I am working');
       }
     }
